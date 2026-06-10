0452539d38e4b667853a654f9788b087a4dc9500
carlomolejon <156200762+carlomolejon@users.noreply.github.com>
2026-06-10 11:18:10 +0800

    Add files via upload

---
+++ Java 1/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        System.out.println("Hello World Again");
+
+        if (a == 2) {
+            System.out.println("Hello World Again2");
+        } else {
+            System.out.println("No Hello World");
+        }
+
+        for (int i = 0; i < 5; i++){
+            System.out.println("Hello World x " + i);
+        }
+    }
+}
